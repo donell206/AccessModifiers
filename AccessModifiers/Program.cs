201840948d55a79619936ddedcffa77d42564c1e
Donnel <donnel206@hotmail.com>
2020-11-13 11:49:16 +0100

    Add project files.

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.SetName("Kenan");
+
+            Console.WriteLine(student.GetName());
+
+            student.SetBirthdate(new DateTime(1987, 05, 21));
+            Console.WriteLine(student.GetBirthDate().ToShortTimeString());
+
+            Console.WriteLine(student.GetAge());
+        }
+
+    }
+}
---
+++ AccessModifiers/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    public class Student
+    {
+        private string _name;
+        const double PI = 3.14;
+        private DateTime _birthdate;
+        private int _age;
+
+        public void SetName(string name)
+        {
+            if (!String.IsNullOrEmpty(name))
+            {
+                this._name = name;
+            }
+        
+         }
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthDate()
+        {
+            return _birthdate;
+        }
+
+        public int GetAge()
+        {
+           TimeSpan span = DateTime.Today - _birthdate;
+            var _age = span.Days/365;
+            return _age;
+        }
+
+
+    }
+}
